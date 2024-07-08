7ae66bb669949f0b20d5c90816e5feff445f5514
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-07-08 22:22:24 +0330

    database config + start express.js server

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+const express = require('express');
+const config = require('./config');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.listen(
+    config.port,
+    () => console.log('Server is listening on http://localhost: ' + config.port)
+)
