9ec20d2d1c29f7874da2f0520a8b587ab330efbe
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-07-15 16:54:41 +0330

    fixed the sql server docker problem

---
+++ data/events/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const utils = require('../utils');
+const config = require('../../config');
+const sql = require('mssql');
+
+const getEvents = async () => {
+    try {
+        let pool = await sql.connect(config.sql);
+        const sqlQueries = await utils.loadSqlQueries('events');
+        const list = await pool.request().query(sqlQueries.eventslist);
+        return list.recordset;
+    } catch (error) {
+        return error.message
+    }
+}
